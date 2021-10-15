f9822bb5d4e8a8a4202e2160a0f490614f22837a
Philip Hayes <phayes@redhat.com>
2021-10-15 10:31:01 -0400

    Added logging

--- kitchensink/extensions/install.sh
+++ kitchensink/extensions/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "Running install.sh"
+echo ${injected_dir}
 if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     set -x
     echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
