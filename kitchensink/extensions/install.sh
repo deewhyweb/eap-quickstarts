3af6858cc09d46ed2b1e48a0e45b8ab601bb6d60
Philip Hayes <philip@deewhy.ie>
2023-05-04 15:18:13 -0400

    Update to shell script

--- kitchensink/extensions/install.sh
+++ kitchensink/extensions/install.sh
@@ -5,6 +5,7 @@ if [ "${SCRIPT_DEBUG}" = "true" ] ; then
     set -x
     echo "Script debugging is enabled, allowing bash commands and their arguments to be printed as they are executed"
 fi
+cp /tmp/src/target/dependency/mysql-connector-j-8.0.33.jar modules/com/mysql/main
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
